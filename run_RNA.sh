77ca05d5adf68057eca54673c77174f81aaebdbb
PathumKossinna <117231371+PathumKossinna@users.noreply.github.com>
2023-10-04 12:17:13 -0400

    updated SLURM script for running pipeline

--- run_RNA.sh
+++ run_RNA.sh
@@ -1,26 +1,66 @@
 #!/usr/bin/env bash
-# -V
-# -cwd
-# -l h_rss=5G
 
-module load star/2.5.2a
-module load R/3.6.1
+#SBATCH --job-name=rna_launcher      
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=youremail@example.com  # Replace with a valid email address or use an argument
+#SBATCH --partition=all          
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4000M
+#SBATCH --time=5-00:00:00
+#SBATCH --output=slurm_outputs/%x_%j.out
+#SBATCH --error=slurm_outputs/%x_%j.out
 
+# Get full paths
 MY_PATH=$(readlink -f ${1})
-echo $MY_PATH
+MY_PIPELINE=$(readlink -f ${2})
+
+# Echo variable paths to standard output
+echo "Processing files in ${MY_PATH}..."
+echo "Using pipeline from ${MY_PIPELINE}..."
+
+MY_CLUSTER_CONFIG=$(readlink -f ${3})
+echo "Submitting to cluster with configuration ${MY_CLUSTER_CONFIG}..."
+
 name="${MY_PATH##*/}"
-echo $name
-cd ${MY_PATH}/fastq
+echo "Processing sample ${name}..."
+
+# Check if the directory exists
+if [[ -d "${MY_PATH}/fastq" ]]; then
+    cd ${MY_PATH}/fastq
+else
+    echo "${MY_PATH}/fastq does not exist. Exiting."
+    exit 1
+fi
+
+# Clear config file or create a new unique one
+> config.yaml
 
 echo "path: ${MY_PATH}/fastq" > config.yaml
 echo "name: ${name}" >> config.yaml
 echo "samples:" >> config.yaml
 
-for file in *R1.fastq; do
-    sample=`basename $file _R1.fastq`
+for file in *R1_001.fastq; do
+    sample=`basename $file _R1_001.fastq`
     echo "- ${sample}" >> config.yaml
 done
 
-MY_SNAKEMAKE=$(which snakemake)
+module load STAR/2.7.9a
+module load R/4.2.1
+module load python3
+module load snakemake/7.3.8
+module load perl/5.30.0
+module load rsem/1.3.0
 
-$MY_SNAKEMAKE -s RNA_pipeline_STAR.mk --configfile ${MY_PATH}/fastq/config.yaml --cluster-config cluster.yaml --cluster "qsub -V -cwd -N hg38_RNA -pe smp 1 -l h_rt={cluster.rt},h_rss={cluster.mem} -p -10 -o qsub_output -e qsub_error" --jobs 1000
+# Check if logs directory exists
+if [[ ! -d "../logs" ]]; then
+    mkdir ../logs
+fi
+
+snakemake -s ${MY_PIPELINE}/RNA_pipeline_STAR.mk \
+          --configfile ${MY_PATH}/fastq/config.yaml \
+          --cluster-config ${MY_CLUSTER_CONFIG} \
+          --nolock \
+          --cluster "sbatch -J {cluster.job-name} -p {cluster.partition} -t {cluster.time} -c {cluster.cpus-per-task} --mem={cluster.mem} -o {cluster.output} -e {cluster.error} --mail-type={cluster.mail-type} --mail-user={cluster.mail-user}" \
+          --jobs 1000
