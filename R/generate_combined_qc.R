79f72ea8d61f1d60263dcf6286d64ce0c0ed18df
PathumKossinna <117231371+PathumKossinna@users.noreply.github.com>
2025-01-14 16:09:14 -0500

    fix combined_qc outputs

--- R/generate_combined_qc.R
+++ R/generate_combined_qc.R
@@ -8,6 +8,7 @@ library(gridExtra)
 args <- commandArgs(TRUE)
 input_files <- snakemake@input
 output_csv <- snakemake@output[["combined_metadata"]]
+output_filtered_csv <- sub("\\.csv$", "_qc_filtered.csv", output_csv)  # Create new filename
 output_plot <- snakemake@output[["combined_plot"]]
 
 # Define a function to read data and assign sample attribute
@@ -22,43 +23,59 @@ read_data <- function(filename) {
 # Read in all data files
 allQC <- do.call(rbind, lapply(input_files, read_data))
 
-# Apply filters
-allQC <- allQC[allQC$nFeature_RNA > 500 & 
-               allQC$percent_mito < 25 & 
-               allQC$percent_ribo < 25 & 
-               allQC$nCount_RNA < quantile(allQC$nCount_RNA, 0.999),]
+# Compute 99.9th percentile for `nCount_RNA`
+count_threshold <- quantile(allQC$nCount_RNA, 0.999)
+message(sprintf("Thresholds: nFeature_RNA > 500, percent_mito < 25, percent_ribo < 25, nCount_RNA < %.2f", count_threshold))
+
+# # Apply filters
+# allQC <- allQC[allQC$nFeature_RNA > 500 & 
+#                allQC$percent_mito < 25 & 
+#                allQC$percent_ribo < 25 & 
+#                allQC$nCount_RNA < quantile(allQC$nCount_RNA, 0.999),]
 
 # Order the factor levels of Sample based on sample names
 allQC$Sample <- factor(allQC$Sample, levels = unique(allQC$Sample), ordered = TRUE)
 
 # Function to create visualizations
-create_plot <- function(y_axis, y_label, y_limit = NULL, y_intercept = NULL) {
-  plot <- ggplot(data = allQC, aes(x=Sample, y = y_axis)) +
-          geom_boxplot(notch = F, outlier.shape = NA) +
-          geom_jitter(alpha = 0.5, width=0.1, cex=1) +
+create_plot <- function(y_axis, y_label, y_limit = NULL, threshold = NULL) {
+  plot <- ggplot(data = allQC, aes(x = Sample, y = y_axis)) +
+          geom_boxplot(outlier.shape = NA) +
+          geom_jitter(alpha = 0.5, width = 0.1, size = 1) +
           labs(y = y_label, x = "Sample") +
-          theme_classic(base_size = 11) + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+          theme_classic(base_size = 11) +
+          theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
   if (!is.null(y_limit)) {
-    plot <- plot + ylim(y_limit)
+    plot <- plot + coord_cartesian(ylim = y_limit)
   }
   
-  if (!is.null(y_intercept)) {
-    plot <- plot + geom_hline(yintercept = y_intercept, linetype="dashed", color = "red")
+  if (!is.null(threshold)) {
+    plot <- plot + geom_hline(yintercept = threshold, linetype = "dashed", color = "red")
   }
   
   return(plot)
 }
 
-plot_nFeature_RNA <- create_plot(allQC$nFeature_RNA, "no. of genes", c(-5, 15000), 500)
-plot_nCount_RNA <- create_plot(allQC$nCount_RNA, "no. of reads")
-plot_percent_mito <- create_plot(allQC$percent_mito, "Percentage mithocondrial genes", c(-5, 105), 25)
-plot_percent_ribo <- create_plot(allQC$percent_ribo, "Percentage ribosomal genes", c(-5, 105), 25)
+
+# Generate plots with QC thresholds
+plot_nFeature_RNA <- create_plot(allQC$nFeature_RNA, "Number of Genes", c(0, 15000), 500)
+plot_nCount_RNA <- create_plot(allQC$nCount_RNA, "Number of Reads", NULL, count_threshold)
+plot_percent_mito <- create_plot(allQC$percent_mito, "Mitochondrial Gene Percentage", c(0, 100), 25)
+plot_percent_ribo <- create_plot(allQC$percent_ribo, "Ribosomal Gene Percentage", c(0, 100), 25)
 
 combined_plot <- grid.arrange(plot_nFeature_RNA, plot_nCount_RNA, plot_percent_mito, plot_percent_ribo)
 
 # Save the combined plot
 ggsave(filename = output_plot, plot = combined_plot, width = 3 + (2 * length(unique(allQC$Sample))), height = 10)
+
+# Apply QC filtering
+filteredQC <- allQC[allQC$nFeature_RNA > 500 & 
+                    allQC$percent_mito < 25 & 
+                    allQC$percent_ribo < 25 & 
+                    allQC$nCount_RNA < count_threshold,]
+
+# Save filtered dataset
+write.csv(filteredQC, file = output_filtered_csv, row.names = FALSE)
 
 # Save the aggregated data
 write.csv(allQC, file = output_csv)
